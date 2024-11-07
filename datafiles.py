e11499382adf4fdf563a835a743a50d64f0d1b88
= <jonhur@web.de>
2024-11-07 11:29:30 +0100

    added field for validator

--- datafiles.py
+++ datafiles.py
@@ -2,42 +2,50 @@ DATAFILES = {
     "gi_jahrgaenge": {
         "data": "data/gi/studium/jahrgaenge.json",
         "template": "data_templates/data/gi/studium/jahrgaenge.json",
-        "schema": "schemas/jahrgaenge.schema.json"
+        "schema": "schemas/jahrgaenge.schema.json",
+        "schema_validator": None
     },
     "gi_praesidienste": {
         "data": "data/gi/start/praesidienste.json",
         "template": "data_templates/data/gi/start/praesidienste.json",
-        "schema": "schemas/praesidienste.schema.json"
+        "schema": "schemas/praesidienste.schema.json",
+        "schema_validator": None
     },
     "gi_termine": {
         "data": "data/gi/start/termine.json",
         "template": "data_templates/data/gi/start/termine.json",
-        "schema": "schemas/termine.schema.json"
+        "schema": "schemas/termine.schema.json",
+        "schema_validator": None
     },
     "gi_rollen": {
         "data": "data/gi/fachschaft/rollen.json",
         "template": "data_templates/data/gi/fachschaft/rollen.json",
-        "schema": "schemas/rollen.schema.json"
+        "schema": "schemas/rollen.schema.json",
+        "schema_validator": None
     },
     "gi_erstiwoche": {
         "data": "data/gi/erstsemester/erstiwoche.json",
         "template": "data_templates/data/gi/erstsemester/erstiwoche.json",
-        "schema": "schemas/erstiwoche.schema.json"
+        "schema": "schemas/erstiwoche.schema.json",
+        "schema_validator": None
     },
     "gi_erstiwochenende": {
         "data": "data/gi/erstsemester/erstiwochenende.json",
         "template": "data_templates/data/gi/erstsemester/erstiwochenende.json",
-        "schema": "schemas/erstiwochenende.schema.json"
+        "schema": "schemas/erstiwochenende.schema.json",
+        "schema_validator": None
     },
     "gi_stundenplan": {
         "data": "data/gi/erstsemester/stundenplan.json",
         "template": "data_templates/data/gi/erstsemester/stundenplan.json",
-        "schema": "schemas/stundenplan.schema.json"
+        "schema": "schemas/stundenplan.schema.json",
+        "schema_validator": None
     },
     "geoloek_praesidienste": {
         "data": "data/geoloek/praesidienste.json",
         "template": "data_templates/data/geoloek/praesidienste.json",
-        "schema": "schemas/praesidienste.schema.json"
+        "schema": "schemas/praesidienste.schema.json",
+        "schema_validator": None
     }
 }
 
