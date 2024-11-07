db935077afdd4bbf1cff43efa4d09e3d181c8227
= <jonhur@web.de>
2024-11-07 11:33:32 +0100

    validate data against schema before creating response

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, make_response
 import json
+
+from jsonschema.exceptions import ValidationError
 import utils
 from os.path import join
 from functools import wraps
@@ -48,14 +50,23 @@ def handle_errors(func):
     def wrapper_func(*args, **kwargs):
         try:
             return func(*args, **kwargs)
+        except json.JSONDecodeError as e:
+            print("File is not a valid JSON file")
+            print(e)
+            return {"error": "File is not a valid json file"}, 500
+        except ValidationError as e:
+            print("JSON file does not conform to its associated JSON-Schema")
+            print(e)
+            return {"error": "File does not validate against its schema"}, 500
         except FileNotFoundError as e:
             print("Could not find specified file")
             print(e)
-            return {"error": "Could not locate a specific file. See console output for details"}
+            return {"error": "Could not locate a specific file. See console output for details"}, 500
         except Exception as e:
             print(e)
             return {"error": "An internal server error has occured."}, 500
     return wrapper_func
+
 
 @app.route("/")
 def hello():
@@ -67,6 +78,7 @@ def hello():
 def erstiwoche():
     with open(DATAFILES["gi_erstiwoche"]["data"]) as file:
         data = json.load(file)
+    utils.validate_dict_againt_schema(DATAFILES["gi_erstiwoche"]["schema_validator"], data)
     return data
 
 
@@ -75,6 +87,7 @@ def erstiwoche():
 def erstiwochenende():
     with open(DATAFILES["gi_erstiwochenende"]["data"]) as file:
         data = json.load(file)
+    utils.validate_dict_againt_schema(DATAFILES["gi_erstiwochenende"]["schema_validator"], data)
     return data
 
 
@@ -83,6 +96,7 @@ def erstiwochenende():
 def stundenplan():
     with open(DATAFILES["gi_stundenplan"]["data"]) as file:
         data = json.load(file)
+    utils.validate_dict_againt_schema(DATAFILES["gi_stundenplan"]["schema_validator"], data)
     return data
 
 
@@ -91,6 +105,7 @@ def stundenplan():
 def rollen():
     with open(DATAFILES["gi_rollen"]["data"]) as file:
         data = json.load(file)
+    utils.validate_dict_againt_schema(DATAFILES["gi_rollen"]["schema_validator"], data)
     return data
 
 
@@ -99,6 +114,7 @@ def rollen():
 def praesidienste():
     with open(DATAFILES["gi_praesidienste"]["data"]) as file:
         data = json.load(file)
+    utils.validate_dict_againt_schema(DATAFILES["gi_praesidienste"]["schema_validator"], data)
     return data
 
 
@@ -107,6 +123,7 @@ def praesidienste():
 def termine():
     with open(DATAFILES["gi_termine"]["data"]) as file:
         data = json.load(file)
+    utils.validate_dict_againt_schema(DATAFILES["gi_termine"]["schema_validator"], data)
     return data
 
 
@@ -115,6 +132,7 @@ def termine():
 def jahrgaenge():
     with open(DATAFILES["gi_jahrgaenge"]["data"]) as file:
         data = json.load(file)
+    utils.validate_dict_againt_schema(DATAFILES["gi_jahrgaenge"]["schema_validator"], data)
     return data
 
 
@@ -168,6 +186,7 @@ def foto_gi_fachschaft():
 def geoloek_praesidienste():
     with open(DATAFILES["geoloek_praesidienste"]["data"]) as file:
         data = json.load(file)
+    utils.validate_dict_againt_schema(DATAFILES["geoloek_praesidienste"]["schema_validator"], data)
     return data
 
 
