5e20f3a7d259e87ff77e1606d4e42ab745b3ec9c
= <jonhur@web.de>
2025-09-28 14:33:32 +0200

    renamed env

--- geofs_website_backend/envs.py
+++ geofs_website_backend/envs.py
@@ -23,21 +23,21 @@ GITHUB_WEBHOOK_SECRET = os.environ.get("GITHUB_WEBHOOK_SECRET", None)
 # Required if github webhook should be used to update website content
 # All required datafiles must exist in repo, otherwise backend will not start
 # Use in the following way: orga/repo, i.e. fs-geofs/geofs-website-content
-GIT_CONTENT_REPO = os.environ.get("GIT_CONTENT_REPO", None)
-if GIT_CONTENT_REPO is not None:
-    valid_repo = _check_github_repo_name(GIT_CONTENT_REPO)
+GITHUB_CONTENT_REPO = os.environ.get("GIT_CONTENT_REPO", None)
+if GITHUB_CONTENT_REPO is not None:
+    valid_repo = _check_github_repo_name(GITHUB_CONTENT_REPO)
     if not valid_repo:
         raise EnvVariableError(
             "GIT_CONTENT_REPO is not a valid env. "
             "Must be format orga/repo, i.e. fs-geofs/geofs-website-content"
         )
 
-    GIT_CONTENT_DIR = GIT_CONTENT_REPO.split("/")[-1]  # name of the repo w/o author
+    GIT_CONTENT_DIR = GITHUB_CONTENT_REPO.split("/")[-1]  # name of the repo w/o author
     GIT_CONTENT_BASE_PATH = ".."  # path in which the repo will be cloned
-    GIT_CONTENT_URL = f"https://github.com/{GIT_CONTENT_REPO}.git"  # repo URL to clone
+    GIT_CONTENT_URL = f"https://github.com/{GITHUB_CONTENT_REPO}.git"  # repo URL to clone
 else:
     GIT_CONTENT_DIR = None
     GIT_CONTENT_BASE_PATH = None
     GIT_CONTENT_URL = None
 
-CONTENT_PATH = "data" if GIT_CONTENT_REPO else f"{GIT_CONTENT_BASE_PATH}/{GIT_CONTENT_DIR}"
+CONTENT_PATH = "data" if GITHUB_CONTENT_REPO else f"{GIT_CONTENT_BASE_PATH}/{GIT_CONTENT_DIR}"
